851c440bf35cb1a5aa06b4983f990443da29752b
elene227 <elelopirtskhalva@gmail.com>
2024-09-23 11:18:21 -0700

    commit

---
+++ Day 00/classwork/claswork1.py
@@ -0,0 +1,6 @@
+
+name = input("please enter your name here: ")
+age = input("enter your age here: ")
+surname = input("enter your surname here: ")
+
+print("my name is", name + ".my surname is", surname + ".my age is", age)
